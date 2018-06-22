bca66cdd3ac7abba7e2531ba4c3d2a79f26415a2
Bianca Minnaar <bminnaar@gmail.com>
2018-06-22 09:36:45 +0200

    Add Location to Droid

---
+++ BaobabMobile/Droid/Injection/Location.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BaobabMobile.Trunk.Injection;
+
+namespace BaobabMobile.Droid.Injection
+{
+    public class Location : ILocation
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+}
---
+++ BaobabMobile/Droid/Injection/LocationService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Android;
+using Android.Content;
+using Android.Content.PM;
+using Android.Gms.Common;
+using Android.Gms.Location;
+using Android.Support.V4.Content;
+using BaobabMobile.Droid.Injection;
+using BaobabMobile.Trunk.Injection;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(LocationService))]
+namespace BaobabMobile.Droid.Injection
+{
+    public class LocationService : ILocationService<ILocation>
+    {
+		public event EventHandler<LocationUpdatedEventArgs<ILocation>> LocationUpdated;
+        Context context = Android.App.Application.Context;
+        bool isRequestingLocationUpdates;
+
+        public LocationService()
+        {
+            if (ContextCompat.CheckSelfPermission(context, Manifest.Permission.AccessFineLocation) == Permission.Granted)
+            {
+                StartLocationUpdates();
+                isRequestingLocationUpdates = true;
+            }
+            else
+            {
+                isRequestingLocationUpdates = false;
+            }
+        }
+
+        bool IsGooglePlayServicesInstalled()
+        {
+            var queryResult = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(context);
+            if (queryResult == ConnectionResult.Success)
+            {
+                //Log.Info("MainActivity", "Google Play Services is installed on this device.");
+                return true;
+            }
+
+            if (GoogleApiAvailability.Instance.IsUserResolvableError(queryResult))
+            {
+                // Check if there is a way the user can resolve the issue
+                var errorString = GoogleApiAvailability.Instance.GetErrorString(queryResult);
+                //Log.Error("MainActivity", "There is a problem with Google Play Services on this device: {0} - {1}",
+                          //queryResult, errorString);
+
+                // Alternately, display the error to the user.
+            }
+
+            return false;
+        }
+
+        public void StartLocationUpdates()
+        {
+            if (IsGooglePlayServicesInstalled())
+            {
+                FusedLocationProviderClient fusedLocationProviderClient = LocationServices.GetFusedLocationProviderClient(context);
+                Android.Locations.Location location = fusedLocationProviderClient.GetLastLocationAsync().Result;
+                LocationUpdated(this, new LocationUpdatedEventArgs<ILocation>(new Location()
+                {
+                    Lat = location.Latitude,
+                    Lon = location.Longitude
+                }));
+            }
+        }
+    }
+}
