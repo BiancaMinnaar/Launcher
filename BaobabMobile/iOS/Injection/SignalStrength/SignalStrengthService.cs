9a8764760007a33431c1fbeccdcee5097b009c7a
Bianca Minnaar <bminnaar@gmail.com>
2018-06-26 07:41:14 +0200

    Add SignalStrengthService to iOS - Add Dependency attribute to Droid SignalStrengthService - Add SignalStrengthService to iOS

--- BaobabMobile/iOS/Injection/SignalStrength/SignalStrengthService.cs
+++ BaobabMobile/iOS/Injection/SignalStrength/SignalStrengthService.cs
@@ -1,26 +1,26 @@
 ﻿using System;
-using BaobabMobile.Droid.Injection.SignalStrength;
+using BaobabMobile.iOS.Injection.SignalStrength;
 using BaobabMobile.Trunk.Injection.SignalStrength;
 using Plugin.Connectivity.Abstractions;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(SignalStrengthService))]
-namespace BaobabMobile.Droid.Injection.SignalStrength
+namespace BaobabMobile.iOS.Injection.SignalStrength
 {
     public class SignalStrengthService : ISignalStrengthService<ISignalSrength>
     {
         public event EventHandler<SignalStrengthUpdatedEventArgs<ISignalSrength>> SignalStrengthChanged;
 
-        public int SignalStrength 
-        { 
-            get => throw new NotImplementedException(); 
-            set => throw new NotImplementedException(); 
+        public int SignalStrength
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
         }
 
-        public int WifiSignalStrength 
-        { 
-            get => throw new NotImplementedException(); 
-            set => throw new NotImplementedException(); 
+        public int WifiSignalStrength
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
         }
 
         public int GetConnectionStrength(ConnectionType connectionType)
