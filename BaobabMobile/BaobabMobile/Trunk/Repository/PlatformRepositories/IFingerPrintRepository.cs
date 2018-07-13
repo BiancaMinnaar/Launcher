1a111fd446d136653ff7d74285cb5034d11dc1df
Bianca Minnaar <bminnaar@gmail.com>
2018-07-13 11:33:19 +0200

    Restructure PlatformRepositories - Add FingerPrintRepository

---
+++ BaobabMobile/BaobabMobile/Trunk/Repository/PlatformRepositories/IFingerPrintRepository.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace BaobabMobile.Trunk.Repository
+{
+    public interface IFingerPrintRepository
+    {
+    }
+}
---
+++ BaobabMobile/BaobabMobile/Trunk/Repository/PlatformRepositories/Implementation/FingerPrintRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BaobabMobile.Trunk.Repository.PlatformRepositories.Implementation
+{
+    public class FingerPrintRepository
+    {
+        public FingerPrintRepository()
+        {
+        }
+    }
+}
