c222e9671399b8da81acbd5f85e1a1c5282533ba
Bianca Minnaar <bminnaar@gmail.com>
2018-07-13 11:27:06 +0200

    Add FingerPrint Scanner plugin to Droid

---
+++ BaobabMobile/Droid/Injection/FingerPrintScanner/FingerPrintService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using BaobabMobile.Droid.Injection.FingerPrintScanner;
+using BaobabMobile.Trunk.Injection.Base;
+using Plugin.Fingerprint;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FingerPrintService))]
+namespace BaobabMobile.Droid.Injection.FingerPrintScanner
+{
+    public class FingerPrintService : PlatformServiceBonsai<IPlatformModelBase>
+    {
+        public override string ServiceKey => "FingerPrintService";
+
+        public override void Activate()
+        {
+            base.Activate();
+            Task.Run(async () => await method());
+        }
+
+        async Task method()
+        {
+            var result = await CrossFingerprint.Current.AuthenticateAsync("Prove you have fingers!");
+            if (result.Authenticated)
+            {
+                // do secret stuff :)
+            }
+            else
+            {
+                // not allowed to do secret stuff :(
+            }
+        }
+    }
+}
