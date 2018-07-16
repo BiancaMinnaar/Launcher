ee4b471c2752d1f42e8dd8a5b93fbe7ddf2cf26b
Bianca Minnaar <bminnaar@gmail.com>
2018-07-16 05:46:10 +0200

    Add NFC Implementation for Droid - Add NFCData - Add NFCService

---
+++ BaobabMobile/Droid/Injection/NFC/NFCData.cs
@@ -0,0 +1,9 @@
+﻿using BaobabMobile.Trunk.Injection.NFC;
+
+namespace BaobabMobile.Droid.Injection.NFC
+{
+    public class NFCData : INFCData
+    {
+        public string ErrorMessage { get; }
+    }
+}
---
+++ BaobabMobile/Droid/Injection/NFC/NFCService.cs
@@ -0,0 +1,9 @@
+﻿using BaobabMobile.Trunk.Injection.Base;
+using BaobabMobile.Trunk.Injection.NFC;
+
+namespace BaobabMobile.Droid.Injection.NFC
+{
+    public class NFCService : PlatformServiceBonsai<IPlatformModelBase>, INFCService<IPlatformModelBase>
+    {
+    }
+}
